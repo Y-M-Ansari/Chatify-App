58934fda611adf7decbe6f5b5691221fc552b1cd
Yousuf Malik Ansari <0603yousuf@gmail.com>
2026-07-06 01:20:57 +0530

    axios file updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -13,7 +13,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : import.meta.env.VITE_API_URL;
+    : `${import.meta.env.VITE_API_URL}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
