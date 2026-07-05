8e8dfb265670d72c30f65fe1d78b65b38fb19ce3
Yousuf Malik Ansari <0603yousuf@gmail.com>
2026-07-06 00:42:57 +0530

    Axios file updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,21 @@
+// This(given below axios code) is for when frontend and backend deployment takes place on same servers
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+//   withCredentials: true,
+// });
+
+// This(given below axios code) is for netlify deployment when frontend and backend deployed on different servers
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
